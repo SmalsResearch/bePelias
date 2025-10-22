83f7d22ddb32eb0b8fa35ab052d16d48aa9df34c
Vandy Berten <vandy.berten@smals.be>
2025-10-22 09:13:43 +0000

    build: git action, update manual.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 ACTION=${1:-"all"}
 
-
-# mkdir testmachin
 # This script runs on the host machine. It builds Pelias and bePelias and run them
 echo "Starting build.sh..."
 
 echo "ACTION: $ACTION"
 date
-
-echo "user: $USER"
-pwd
 
 DIR=pelias/projects/belgium_bepelias
 
@@ -80,10 +75,6 @@ if [[ $ACTION == "pelias" ||  $ACTION == "all" ]]; then
     #mkdir -p data
     # chmod a+wr data
     echo 'DATA_DIR=./data' >> .env
-
-    # echo "Create group & user" 
-    # groupadd -g 10001 pelias && \
-    # useradd -u 10000 -g pelias pelias 
 
     $PELIAS compose pull
     $PELIAS elastic start
